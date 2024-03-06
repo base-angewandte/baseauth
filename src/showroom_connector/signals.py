4e9da4542d7d952ad79fb49f0a5c9d13114b6036
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2024-03-06 17:10:05 +0100

    feat: create user preferences data on user save

--- src/showroom_connector/signals.py
+++ src/showroom_connector/signals.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 
 from user_preferences.models import UserPreferencesData, UserSettingsValue
@@ -22,4 +23,16 @@ post_save.connect(
     handle_post_save,
     sender=UserSettingsValue,
     dispatch_uid='post_save_usersettingsvalue',
+)
+
+
+def create_user_preferences(sender, instance, *args, **kwargs):
+    """Make sure that a user has a related UserPreferenceData object."""
+    UserPreferencesData.objects.get_or_create(user=instance)
+
+
+post_save.connect(
+    create_user_preferences,
+    sender=get_user_model(),
+    dispatch_uid='post_save_create_userpreferencesdata',
 )
