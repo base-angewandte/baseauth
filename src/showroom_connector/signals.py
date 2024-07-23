b6d5755c59e4892c3d80689e596e2cb13277e6af
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2024-07-23 15:19:30 +0200

    refactor: use @receiver decorator to connect signal handlers

--- src/showroom_connector/signals.py
+++ src/showroom_connector/signals.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from user_preferences.models import UserPreferencesData, UserSettingsValue
 
@@ -8,31 +9,27 @@ from .rq import enqueue
 from .sync import push_user
 
 
+@receiver(
+    post_save,
+    sender=UserPreferencesData,
+    dispatch_uid='post_save_userpreferencesdata',
+)
+@receiver(
+    post_save,
+    sender=UserSettingsValue,
+    dispatch_uid='post_save_usersettingsvalue',
+)
 def handle_post_save(sender, instance, *args, **kwargs):
     if settings.SYNC_TO_SHOWROOM:
         job_id = f'push_user_{instance.user.username}'
         enqueue(job_id, push_user, user=instance.user)
 
 
-post_save.connect(
-    handle_post_save,
-    sender=UserPreferencesData,
-    dispatch_uid='post_save_userpreferencesdata',
-)
-post_save.connect(
-    handle_post_save,
-    sender=UserSettingsValue,
-    dispatch_uid='post_save_usersettingsvalue',
-)
-
-
-def create_user_preferences(sender, instance, *args, **kwargs):
-    """Make sure that a user has a related UserPreferenceData object."""
-    UserPreferencesData.objects.get_or_create(user=instance)
-
-
-post_save.connect(
-    create_user_preferences,
+@receiver(
+    post_save,
     sender=get_user_model(),
     dispatch_uid='post_save_create_userpreferencesdata',
 )
+def create_user_preferences(sender, instance, *args, **kwargs):
+    """Make sure that a user has a related UserPreferenceData object."""
+    UserPreferencesData.objects.get_or_create(user=instance)
