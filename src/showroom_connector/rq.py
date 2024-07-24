c2e3151de9fbbe2ab364ca1d7f6fc1bb798da5f4
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2024-07-24 14:34:51 +0200

    docs: annotate enqueue wrapper function

--- src/showroom_connector/rq.py
+++ src/showroom_connector/rq.py
@@ -8,6 +8,14 @@ from django.conf import settings
 
 
 def enqueue(job_id, function, **kwargs):
+    """Wrapper function to enqueue worker jobs only once and with a delay.
+
+    In cases of mass user updates, or when a user updates their settings
+    in several quick consecutive API calls, we only want to enqueue the
+    sync of the user once. Therefore, we delete prior existing jobs to
+    sync the same user, before we freshly enqueue it with a configurable
+    delay.
+    """
     queue = get_queue('default')
     registry = ScheduledJobRegistry(queue=queue)
     if job_id in registry.get_job_ids():
