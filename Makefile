9d1ad4a7ceb1eac351cbfb43ad9c8387451cb964
jackie / Andrea Ida Malkah Klaura <andrea.klaura@uni-ak.ac.at>
2025-05-14 12:53:15 +0200

    change: use make command and PROJECT_NAME for script

--- Makefile
+++ Makefile
@@ -9,3 +9,7 @@ start-dev:  ## start containers for local development
 	docker-compose up -d --build \
 		${PROJECT_NAME}-redis \
 		${PROJECT_NAME}-postgres
+
+.PHONY: migrate-user-model
+migrate-user-model:  ## migrate user model from Django default to custom model
+	@bash scripts/migrate-user-model.sh
--- scripts/migrate-user-model.sh
+++ scripts/migrate-user-model.sh
@@ -2,7 +2,14 @@
 
 set -e
 
-if ! docker compose exec baseauth-postgres sh -c 'echo "SELECT * FROM django_migrations" | psql -U django_baseauth' | grep accounts | grep -q 0001_initial; then
+if [ -z "${PROJECT_NAME}" ]; then
+	echo "ERROR: this script has to be called through the make command: make migrate-user-model"
+	exit
+fi
+
+QUERY="echo \"SELECT * FROM django_migrations\" | psql -U django_${PROJECT_NAME}"
+
+if ! docker compose exec baseauth-postgres sh -c "${QUERY}" | grep accounts | grep -q 0001_initial; then
 	docker compose exec -T baseauth-postgres psql -U django_baseauth <"$(dirname "$0")/migrate-user-model.sql"
 else
 	echo 'Initial accounts migration is already applied.'
